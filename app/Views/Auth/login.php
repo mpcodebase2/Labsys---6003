cc719a6364d55fe74c635a915985ed371c3613b2
mpcodebase2 <mpranav3000@gmail.com>
2024-03-18 08:06:39 +0530

    views misc

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -7,7 +7,7 @@
         <div class="col-12 p-0">
             <div class="login-card login-dark">
                 <div>
-                    <div><a class="logo" href="<?=base_url()?>"><img class="img-fluid for-light" src="<?=base_url()?>/assets/images/logo/logo.png" alt="looginpage"><img class="img-fluid for-dark" src="../assets/images/logo/logo_dark.png" alt="looginpage"></a></div>
+                    <div><a class="logo" href="<?=base_url()?>admin"><img class="img-fluid for-light" src="<?=base_url()?>/assets/images/logo/logo.png" alt="looginpage"><img class="img-fluid for-dark" src="../assets/images/logo/logo_dark.png" alt="looginpage"></a></div>
                     <div class="login-main">
                         <form class="theme-form" method="post" action="<?=base_url()?>admin/api/login">
                             <h4>Sign in to account</h4>
@@ -32,12 +32,11 @@
                                 <div class="checkbox p-0">
                                     <input id="checkbox1" type="checkbox">
                                     <label class="text-muted" for="checkbox1">Remember password</label>
-                                </div><a class="link" href="">Forgot password?</a>
+                                </div>
                                 <div class="text-end mt-3">
                                     <button class="btn btn-primary btn-block w-100" type="submit">Sign in</button>
                                 </div>
                             </div>
-                            <p class="mt-4 mb-0 text-center">Don't have account?<a class="ms-2" href="<?=base_url()?>admin/sign-up">Create Account</a></p>
                         </form>
                     </div>
                 </div>
